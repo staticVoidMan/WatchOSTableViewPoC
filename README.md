cbb796171984ff18d84cea10c11bd659ee0a41ae
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-12-29 12:26:30 +0530

    - Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WatchOS Project: TableView
+
---
+++ WatchOSTableViewPoC WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchOSTableViewPoC WatchKit Extension
+//
+//  Created by Amin Siddiqui on 29/12/17.
+//  Copyright © 2017 svmLogics. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
